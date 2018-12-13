9ab95697c4f264748997f71e523b883a681d002b
Lexae <atparky@outlook.fr>
2018-12-13 10:49:39 +0100

    premier commit

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+const second = 1000,
+  minute = second * 60,
+  hour = minute * 60,
+  day = hour * 24;
+
+let countDown = new Date('Jan 29, 2019 09:00:00').getTime(),
+  x = setInterval(function() {
+
+    let now = new Date().getTime(),
+      distance = countDown - now;
+
+    document.getElementById('days').innerText = Math.floor(distance / (day)),
+      document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+      document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+      document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+
+  }, second)
+
+
+
+$(function() {
+  var header = $(".navInovia");
+
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 50) {
+      header.addClass("scrolled");
+    } else {
+      header.removeClass("scrolled");
+    }
+  });
+
+});
